214a6b67e5a072a998fa9442ce585d40161d3c04
Fabian van den Berg <78847718+FabianCGBerg@users.noreply.github.com>
2021-02-20 16:16:17 +0100

    Add files via upload

--- Data Extraction Numeracy App/Data_Extraction_Numeracy.py
+++ Data Extraction Numeracy App/Data_Extraction_Numeracy.py
@@ -1,77 +1,71 @@
-
 from os import path, listdir
 from os.path import isfile, join
 from pathlib import Path
 import inspect
-
+from typing import Any, Union
 import pandas as pd
 import Numeracy_App_Script
 
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 #       Behavioral Settings
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 
 filename = Path(path.dirname(path.abspath(inspect.getframeinfo(inspect.currentframe()).filename)))
 
-BehavioralSettings = []
+BehavioralSettings: list[Union[Union[Path, str], Any]] = [(Path(filename) / 'data'), (Path(filename) / 'Results'),
+                                                          'Numeracy_Data.xlsx']
 
-BehavioralSettings.append( (Path(filename) / 'data')); #0 LoadPath
-BehavioralSettings.append( (Path(filename) / 'Results')); #1 SavePath
-BehavioralSettings.append('Numeracy_Data.xlsx'); #2 ExcelFile
-
-#make sure the results folder exists, if not it is created
+# make sure the results folder exists, if not it is created
 BehavioralSettings[1].mkdir(parents=True, exist_ok=True)
-    
-NumberOfErrors = 0 #Initialize error counter
-AllDataUni = pd.DataFrame()  #initialize dataframes
 
-#Load in all the .txt files in that directory
+NumberOfErrors = 0  # Initialize error counter
+AllDataUni = pd.DataFrame()  # initialize dataframes
+
+# Load in all the .dat files in that directory
 Files = []
 for file in listdir(Path(BehavioralSettings[0])):
     if file.endswith(".txt"):
         Files.append(file)
-    
-#Loop through the files and extract the data
-for File in Files: 
+
+# Loop through the files and extract the data
+for File in Files:
     print('processing: ' + File)
     try:
-        DataUni = Numeracy_App_Script.Numeracy_App_Data_Runner(join(BehavioralSettings[0], File))
-        AllDataUni = AllDataUni.append(DataUni, ignore_index = True)
+        DataUni = Numeracy_App_Script.numeracy_app_data_runner(join(BehavioralSettings[0], File))
+        AllDataUni = AllDataUni.append(DataUni, ignore_index=True)
     except:
-        print('Error occured at file: ' + File)
+        print('Error occurred at file: ' + File)
         NumberOfErrors += 1
-    
+
+
 if NumberOfErrors == 0:
-        
+
     ExcelFileName = join(BehavioralSettings[1], BehavioralSettings[2])
-    
-    Measures = [['NumCncpt', 'CountSub', 'Pattern', 'MatchNum', 'WordProb', 'NumLine', 'Subit', 'SpMeas', 'OrdPos', 'Equat'],
-                        ['Early Numerical Concepts and Language', 
-                        'Counting a Subset', 
-                        'Discerning & Completing Patterns', 
-                        'Matching Digit & Quantity', 
-                        'Numerical Word Problems', 
-                        'Completing Number Line', 
-                        'Conceptual Subitizing', 
-                        'Early Spatial & Measurement Concepts', 
-                       'Ordinal Position', 
-                       'Number Comparison']
-                       ]
+
+    Measures = [
+        ['NumCncpt', 'CountSub', 'Pattern', 'MatchNum', 'WordProb', 'NumLine', 'Subit', 'SpMeas', 'OrdPos', 'Equat'],
+        ['Early Numerical Concepts and Language',
+         'Counting a Subset',
+         'Discerning & Completing Patterns',
+         'Matching Digit & Quantity',
+         'Numerical Word Problems',
+         'Completing Number Line',
+         'Conceptual Subitizing',
+         'Early Spatial & Measurement Concepts',
+         'Ordinal Position',
+         'Number Comparison']
+    ]
 
     Measures = pd.DataFrame(data=Measures).T
-        
+
     if isfile(ExcelFileName):
-        Numeracy_App_Script.write_excel(ExcelFileName,('Numeracy Data'),AllDataUni)
+        Numeracy_App_Script.write_excel(ExcelFileName, 'Numeracy Data', AllDataUni)
     else:
-        with pd.ExcelWriter(ExcelFileName, mode='w' ) as writer: 
-            Measures.to_excel(writer, sheet_name='Measures') 
-            AllDataUni.to_excel(writer, sheet_name=('Numeracy Data'))
-        
+        with pd.ExcelWriter(ExcelFileName, mode='w') as writer:
+            Measures.to_excel(writer, sheet_name='Measures')
+            AllDataUni.to_excel(writer, sheet_name='Numeracy Data')
+
 print('')
 print('***************************************')
-print('Succesfully saved Numeracy Data to file: ' + BehavioralSettings[2])
+print('Successfully saved Numeracy Data to file: ' + BehavioralSettings[2])
 print('***************************************')
-
-
-
-
--- Data Extraction Numeracy App/Numeracy_App_Script.py
+++ Data Extraction Numeracy App/Numeracy_App_Script.py
@@ -1,84 +1,92 @@
+import warnings
+warnings.simplefilter(action='ignore', category=FutureWarning)
 import pandas as pd
 import numpy as np
 
-def write_excel(filename,sheetname,dataframe):
-    #function that checks if a worksheet exists, deletes it, and then writes the data to it
-    with pd.ExcelWriter(filename, engine='openpyxl', mode='a') as writer: 
-        workBook = writer.book
+
+# noinspection PyBroadException
+def write_excel(filename, sheetname, dataframe):
+    # function that checks if a worksheet exists, deletes it, and then writes the data to it
+    with pd.ExcelWriter(filename, engine='openpyxl', mode='a') as writer:
+        work_book = writer.book
         try:
-            workBook.remove(workBook[sheetname])
+            work_book.remove(work_book[sheetname])
         except:
             print("Worksheet does not exist")
         finally:
-            dataframe.to_excel(writer, sheet_name=sheetname,index=False)
+            dataframe.to_excel(writer, sheet_name=sheetname, index=False)
             writer.save()
 
-def CheckLines(FileName, LookUp):
-    # Fucntion to find the line number of a certain string
-    with open(FileName) as DataFile:
+
+def check_lines(file_name, look_up):
+    # Function to find the line number of a certain string
+    with open(file_name) as DataFile:
         for num, line in enumerate(DataFile, 1):
-            if LookUp in line:
-                DataFile.close
+            if look_up in line:
+                DataFile.close()
                 return num
-                break
 
-def Numeracy_App_Data_Runner(filename):
+
+def numeracy_app_data_runner(filename):
     # we look for the first row of the actual data using the "trial_Number" keyword
-    DataStart = CheckLines(filename, 'Notes:')
-    DataEnd = CheckLines(filename, 'Score')
+    data_start = check_lines(filename, 'Notes:')
+    data_end = check_lines(filename, 'Score')
 
-    #extract the participant information from the file, this is always at the top
+    # extract the participant information from the file, this is always at the top
     # of the file, 6 rows before the data starts
-    rows_to_keep = range(3, DataStart-2)
+    rows_to_keep = range(3, data_start - 2)
 
-    #Import only those rows into a Dataframe
-    Info_DF = pd.read_csv(filename, skiprows = lambda x: x not in rows_to_keep, header=None, sep=":")
-    Info_DF = Info_DF.T                         #transpose the dataframe so we can call on the info by column name
+    # Import only those rows into a Dataframe
+    info_df = pd.read_csv(filename, skiprows=lambda x: x not in rows_to_keep, header=None, sep=":")
+    info_df = info_df.T  # transpose the dataframe so we can call on the info by column name
 
     # in order to call the information by name (Such as DF_Info['Participant']) we replace the headers
-    new_header = Info_DF.iloc[0]        #grab the first row for the header
-    Info_DF = Info_DF[1:]                       #take the data less the header row
-    Info_DF.columns = new_header    #set the header row as the df header
+    new_header = info_df.iloc[0]  # grab the first row for the header
+    info_df = info_df[1:]  # take the data less the header row
+    info_df.columns = new_header  # set the header row as the df header
 
-    #Now we can read in the data, we skip the rows until we get to the actual data and stop before the scores given by the app
-    rows_to_keep = range(DataStart+1, DataEnd-2)
+    # Now we can read in the data
+    # we skip the rows until we get to the actual data and stop before the scores given by the app
 
-    Data_DF = pd.read_csv(filename, 
-                                            skiprows = lambda x: x not in rows_to_keep, 
-                                            header=None, 
-                                            sep="\\.\\ | \\_\\ | Answer: | Response", 
-                                            engine='python')
+    rows_to_keep = range(data_start + 1, data_end - 2)
 
-    Data_DF.columns = ['Trials', 'Component', 'Correct', 'Response']
-    Data_DF['Component'] = Data_DF['Component'].str.replace('\d+', '') #removing the numbers from the Component labels
-    Data_DF['Component'] = Data_DF['Component'].str.replace('_', '') #removing the numbers from the Component labels
+    data_df = pd.read_csv(filename,
+                          skiprows=lambda x: x not in rows_to_keep,
+                          header=None,
+                          sep="\\.\\ | \\_\\ | Answer: | Response",
+                          engine='python')
 
-    #Total is the total number of times a component appears in the data
-    ScoreTotal  = Data_DF.groupby('Component').size()
+    data_df.columns = ['Trials', 'Component', 'Correct', 'Response']
+    data_df['Component'] = data_df['Component'].str.replace('\\d+', '')  # removing the numbers from the labels
+    data_df['Component'] = data_df['Component'].str.replace('_', '')  # removing the numbers from the Component labels
 
-    #The score is the number of components that is still there when we only look at correct responses
-    ScoreCorrect  = Data_DF[Data_DF['Correct']==1 ].groupby('Component').sum()
+    # Total is the total number of times a component appears in the data
+    score_total = data_df.groupby('Component').size()
 
-    #we can combine these in a single dataframe to make it easier 
-    Scores = pd.concat([ScoreCorrect['Correct'], ScoreTotal], axis=1)
-    Scores.columns = ['Correct', 'Total']
+    # The score is the number of components that is still there when we only look at correct responses
+    score_correct = data_df[data_df['Correct'] == 1].groupby('Component').sum()
 
-    Scores['Percentage'] = Scores['Correct']/Scores['Total']
+    # we can combine these in a single dataframe to make it easier
+    scores = pd.concat([score_correct['Correct'], score_total], axis=1)
+    scores.columns = ['Correct', 'Total']
 
-    dataUni =  {'PP'                   : np.concatenate([np.repeat(Info_DF['Child ID'], len(Scores) )]), 
-                        'Grade'               : np.concatenate([np.repeat(Info_DF['Grade'], len(Scores) )]),
-                        'Birthday'               : np.concatenate([np.repeat(Info_DF['Date Of Birth'], len(Scores) )]),
-                        'Sex'               : np.concatenate([np.repeat(Info_DF['Sex'], len(Scores) )]),
-                        'Age'               : np.concatenate([np.repeat(Info_DF['Age'], len(Scores) )]),
-                        }
+    scores['Percentage'] = scores['Correct'] / scores['Total']
 
-    BehavioralDataUni = pd.DataFrame(dataUni)
-    BehavioralDataUni['Component'] = Scores.index.values
-    BehavioralDataUni['Correct'] = Scores['Correct'].values
-    BehavioralDataUni['Total'] = Scores['Total'].values
-    BehavioralDataUni['Percentage'] = Scores['Percentage'].values
+    data_uni = {'PP': np.concatenate([np.repeat(info_df['Child ID'], len(scores))]),
+                'Grade': np.concatenate([np.repeat(info_df['Grade'], len(scores))]),
+                'Birthday': np.concatenate([np.repeat(info_df['Date Of Birth'], len(scores))]),
+                'Sex': np.concatenate([np.repeat(info_df['Sex'], len(scores))]),
+                'Age': np.concatenate([np.repeat(info_df['Age'], len(scores))]),
+                }
 
-    return BehavioralDataUni
+    behavioral_data_uni = pd.DataFrame(data_uni)
+    behavioral_data_uni['Component'] = scores.index.values
+    behavioral_data_uni['Correct'] = scores['Correct'].values
+    behavioral_data_uni['Total'] = scores['Total'].values
+    behavioral_data_uni['Percentage'] = scores['Percentage'].values
 
-Testfile = '/Users/fabian/Dropbox/Work/Python Tasks/Numeracy App/data/1_Numeracy.txt'
-print(Numeracy_App_Data_Runner(Testfile))
+    return behavioral_data_uni
+
+
+#test_file = '/Users/fabian/Dropbox/Work/Python Tasks/Data Extraction Numeracy App/data/1_Numeracy.txt'
+#numeracy_app_data_runner(test_file)
