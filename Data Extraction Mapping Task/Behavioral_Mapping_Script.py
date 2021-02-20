bb1f8d3767155b3d5b2e61b641030a1d9b9f680f
Fabian van den Berg <78847718+FabianCGBerg@users.noreply.github.com>
2021-02-20 16:34:19 +0100

    Add files via upload

--- Data Extraction Mapping Task/Behavioral_Mapping_Script.py
+++ Data Extraction Mapping Task/Behavioral_Mapping_Script.py
@@ -1,98 +1,98 @@
-import csv
 import pandas as pd
 import numpy as np
 
-def write_excel(filename,sheetname,dataframe):
-    #function that checks if a worksheet exists, deletes it, and then writes the data to it
-    with pd.ExcelWriter(filename, engine='openpyxl', mode='a') as writer: 
-        workBook = writer.book
+
+def write_excel(filename, sheetname, dataframe):
+    # function that checks if a worksheet exists, deletes it, and then writes the data to it
+    with pd.ExcelWriter(filename, engine='openpyxl', mode='a') as writer:
+        work_book = writer.book
         try:
-            workBook.remove(workBook[sheetname])
+            work_book.remove(work_book[sheetname])
         except:
             print("Worksheet does not exist")
         finally:
-            dataframe.to_excel(writer, sheet_name=sheetname,index=False)
+            dataframe.to_excel(writer, sheet_name=sheetname, index=False)
             writer.save()
 
-def CheckLines(FileName, LookUp):
-    # Fucntion to find the line number of a certain string
-    with open(FileName) as DataFile:
+
+def check_lines(file_name, look_up):
+    # Function to find the line number of a certain string
+    with open(file_name) as DataFile:
         for num, line in enumerate(DataFile, 1):
-            if LookUp in line:
-                DataFile.close
+            if look_up in line:
+                DataFile.close()
                 return num
-                break
 
 
-def Behavioral_Mapping_Runner(filename):
-    #filename = "/Users/fabian/Dropbox/Work/Python Tasks/Mapping Task/Python Scripts/07 - Raw Data/Mapping Task/Canonical/ppn002_MappingTask_2021-02-10T10.28.40.dat"
-
+def behavioral_mapping_runner(filename):
     # we look for the first row of the actual data using the "trial_Number" keyword
-    DataStart = CheckLines(filename, 'Trial_Number')
+    data_start = check_lines(filename, 'Trial_Number')
 
-    #e xtract the participant information from the file, this is always at the top
+    # extract the participant information from the file, this is always at the top
     # of the file, 6 rows before the data starts
-    rows_to_keep = range(1, DataStart-6)
+    rows_to_keep = range(1, data_start - 6)
 
-    #Import only those rows into a Dataframe
-    Info_DF = pd.read_csv(filename, skiprows = lambda x: x not in rows_to_keep, header=None, sep=";")
-    Info_DF = Info_DF.T                         #transpose the dataframe so we can call on the info by column name
+    # Import only those rows into a Dataframe
+    info_df = pd.read_csv(filename, skiprows=lambda x: x not in rows_to_keep, header=None, sep=";")
+    info_df = info_df.T  # transpose the dataframe so we can call on the info by column name
 
     # in order to call the information by name (Such as DF_Info['Participant']) we replace the headers
-    new_header = Info_DF.iloc[0]        #grab the first row for the header
-    Info_DF = Info_DF[1:]                       #take the data less the header row
-    Info_DF.columns = new_header    #set the header row as the df header
+    new_header = info_df.iloc[0]  # grab the first row for the header
+    info_df = info_df[1:]  # take the data less the header row
+    info_df.columns = new_header  # set the header row as the df header
 
-    #Now we can read in the data, we skip the rows until we get to the actual data
+    # Now we can read in the data, we skip the rows until we get to the actual data
     # Columns are as follows:
     # 0) Block; 1) Trial_Number; 2) Number Shown; 3) Key Pressed;
     # 4) Correct; 5) RT; 6) Mirror
-    Data_DF = pd.read_csv(filename, skiprows = DataStart-1, sep=";")
+    data_df = pd.read_csv(filename, skiprows=data_start - 1, sep=";")
 
-    #we create a new column telling us if the number they saw was larger than 5
-    Data_DF['LowHigh'] = np.where(Data_DF['Number Shown']>= 5, 1, 0)
+    # we create a new column telling us if the number they saw was larger than 5
+    data_df['LowHigh'] = np.where(data_df['Number Shown'] >= 5, 1, 0)
 
     # then we calculate the average Accuracy and RT for Low and High
-    AccOverall = Data_DF.groupby('LowHigh')['Correct'].mean()
-    RTOverall = Data_DF.groupby('LowHigh')['RT'].mean()
+    acc_overall = data_df.groupby('LowHigh')['Correct'].mean()
+    rt_overall = data_df.groupby('LowHigh')['RT'].mean()
 
-    #Lastly, we calculate the RTs on only correct responses for Low and High
-    RTCorrect  = Data_DF[Data_DF['Correct']==1 ].groupby('LowHigh')['RT'].mean()
+    # Lastly, we calculate the RTs on only correct responses for Low and High
+    rt_correct = data_df[data_df['Correct'] == 1].groupby('LowHigh')['RT'].mean()
 
-
-    #Then we can create the output
+    # Then we can create the output
 
     # in a multivariate format (1 row)
-    dataMult = {'PP'                   : Info_DF['Participant'], 
-                    'Options'               : Info_DF['Options'],
-                    'Stimulus'              : Info_DF['Stimulus'],
-                    'Acc_Overall'         : AccOverall.mean(),
-                    'Acc_Low'              :AccOverall[0],
-                    'Acc_High'             :AccOverall[1],
-                    'RT_Overall'           :RTOverall.mean(),
-                    'RT_Low'               :RTOverall[0],
-                    'RT_High'              :RTOverall[1],
-                    'RT_Cor_Overall'   :RTCorrect.mean(),
-                    'RT_Cor_Low'        :RTCorrect[0],
-                    'RT_Cor_High'       :RTCorrect[1]
-                    } 
+    data_mult = {'PP': info_df['Participant'],
+                 'Options': info_df['Options'],
+                 'Stimulus': info_df['Stimulus'],
+                 'Acc_Overall': acc_overall.mean(),
+                 'Acc_Low': acc_overall[0],
+                 'Acc_High': acc_overall[1],
+                 'RT_Overall': rt_overall.mean(),
+                 'RT_Low': rt_overall[0],
+                 'RT_High': rt_overall[1],
+                 'RT_Cor_Overall': rt_correct.mean(),
+                 'RT_Cor_Low': rt_correct[0],
+                 'RT_Cor_High': rt_correct[1]
+                 }
 
-    BehavioralDataMult = pd.DataFrame(dataMult)
+    behavioral_data_mult = pd.DataFrame(data_mult)
 
     # and as a univariate format
-    dataUni =  {'PP'                   : np.concatenate([np.repeat(Info_DF['Participant'], len(AccOverall) )]), 
-                    'Options'               : np.concatenate([np.repeat(Info_DF['Options'], len(AccOverall) )]),
-                    'Stimulus'              : np.concatenate([np.repeat(Info_DF['Stimulus'], len(AccOverall) )]),
-                    'Mean_Acc'           : AccOverall.mean(),
-                    'Mean_RT'             : RTOverall.mean(),
-                    'Mean_RT_Cor'      : RTCorrect.mean(),
-                    'Range'                 : [0, 1],
-                    'Acc'                       : AccOverall,
-                    'RT_Overall'           : RTOverall,
-                    'RT_Correct'          : RTCorrect
-                    }
+    data_uni = {'PP': np.concatenate([np.repeat(info_df['Participant'], len(acc_overall))]),
+                'Options': np.concatenate([np.repeat(info_df['Options'], len(acc_overall))]),
+                'Stimulus': np.concatenate([np.repeat(info_df['Stimulus'], len(acc_overall))]),
+                'Mean_Acc': acc_overall.mean(),
+                'Mean_RT': rt_overall.mean(),
+                'Mean_RT_Cor': rt_correct.mean(),
+                'Range': [0, 1],
+                'Acc': acc_overall,
+                'RT_Overall': rt_overall,
+                'RT_Correct': rt_correct
+                }
 
-    BehavioralDataUni = pd.DataFrame(dataUni)
-    
-    return int(Info_DF['Participant']), BehavioralDataMult, BehavioralDataUni
+    behavioral_data_uni = pd.DataFrame(data_uni)
 
+    return int(info_df['Participant']), behavioral_data_mult, behavioral_data_uni
+
+
+#test_file = '/Users/fabian/Dropbox/Work/Python Tasks/Mapping Task/Data Extraction Mapping Task/07 - Raw Data/Mapping Task/Canonical/ppn003_MappingTask_2021-02-10T10.28.40.dat'
+#behavioral_mapping_runner(test_file)
--- Data Extraction Mapping Task/Data_Extraction_Mapping.py
+++ Data Extraction Mapping Task/Data_Extraction_Mapping.py
@@ -1,84 +1,75 @@
-
 from os import path, listdir
 from os.path import isfile, join
 from pathlib import Path
 import inspect
 
 import pandas as pd
-import Behavioral_Mapping_Script 
+import Behavioral_Mapping_Script
 
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 #       Behavioral Settings
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 
 filename = Path(path.dirname(path.abspath(inspect.getframeinfo(inspect.currentframe()).filename)))
 
-BehavioralSettings = []
+BehavioralSettings = [(Path(filename) / '07 - Raw Data/Mapping Task/'), ['Canonical', 'Non-Canonical'],
+                      Path('07 - Raw Data/Mapping Task/Results/'), 'Mapping_Behavioral_Data.xlsx']
 
-BehavioralSettings.append( (Path(filename) / '07 - Raw Data/Mapping Task/')); #0 LoadPath
-BehavioralSettings.append(['Canonical', 'Non-Canonical']); #1 Folders
-BehavioralSettings.append(Path(BehavioralSettings[0] / 'Results/')); #2 SavePath
-BehavioralSettings.append('Mapping_Behavioral_Data.xlsx'); #3 ExcelFile
-
-#make sure the results folder exists, if not it is created
+# make sure the results folder exists, if not it is created
 BehavioralSettings[2].mkdir(parents=True, exist_ok=True)
 
 for LoopDir in BehavioralSettings[1]:
-    
+
     print('==========================')
     print('processing Folder: ' + LoopDir)
     print('==========================')
-    
-    NumberOfErrors = 0 #Initialize error counter
-    
-    AllDataUni = pd.DataFrame()  #initialize dataframes
-    AllDataMulti= pd.DataFrame()
-    
-    #Create the current directory
+
+    NumberOfErrors = 0  # Initialize error counter
+
+    AllDataUni = pd.DataFrame()  # initialize dataframes
+    AllDataMulti = pd.DataFrame()
+
+    # Create the current directory
     CurDir = Path(BehavioralSettings[0] / LoopDir)
-    
-    #Load in all the .dat files in that directory
+
+    # Load in all the .dat files in that directory
     Files = []
     for file in listdir(CurDir):
         if file.endswith(".dat"):
             Files.append(file)
-    
-    #Loop through the files and extract the data
-    for File in Files: 
+
+    # Loop through the files and extract the data
+    for File in Files:
         print('processing: ' + File)
         try:
-            PPNum, DataMult, DataUni = Behavioral_Mapping_Script.Behavioral_Mapping_Runner(join(CurDir, File))
-            AllDataUni = AllDataUni.append(DataUni, ignore_index = True)
-            AllDataMulti = AllDataMulti.append(DataMult, ignore_index = True)
+            PPNum, DataMult, DataUni = Behavioral_Mapping_Script.behavioral_mapping_runner(join(CurDir, File))
+            AllDataUni = AllDataUni.append(DataUni, ignore_index=True)
+            AllDataMulti = AllDataMulti.append(DataMult, ignore_index=True)
         except:
-             print('Error occured at file: ' + File)
-             NumberOfErrors += 1
-    
+            print('Error occurred at file: ' + File)
+            NumberOfErrors += 1
+
     if NumberOfErrors == 0:
-        
+
         ExcelFileName = join(BehavioralSettings[2], BehavioralSettings[3])
-        
+
         BinData = {'Options': ['1: Numbers', '2: Canonical', '3: Non-Canonical'],
-                    'Stimulus': ['1: Numbers', '2: Canonical', '3: Non-Canonical'],
-                    'Low_High': ['0: Low', '1: High', '']
-                    };
-        
+                   'Stimulus': ['1: Numbers', '2: Canonical', '3: Non-Canonical'],
+                   'Low_High': ['0: Low', '1: High', '']
+                   }
+
         Bins = pd.DataFrame(data=BinData)
-        
+
         if isfile(ExcelFileName):
-            Behavioral_Mapping_Script.write_excel(ExcelFileName,('Univariate_' + LoopDir),AllDataUni)
-            Behavioral_Mapping_Script.write_excel(ExcelFileName,('Multivariate_' + LoopDir),AllDataMulti)
+            Behavioral_Mapping_Script.write_excel(ExcelFileName, ('Univariate_' + LoopDir), AllDataUni)
+            Behavioral_Mapping_Script.write_excel(ExcelFileName, ('Multivariate_' + LoopDir), AllDataMulti)
         else:
-            with pd.ExcelWriter(ExcelFileName, mode='w' ) as writer: 
-                Bins.to_excel(writer, sheet_name='Bins') 
+            with pd.ExcelWriter(ExcelFileName, mode='w') as writer:
+                Bins.to_excel(writer, sheet_name='Bins')
                 AllDataUni.to_excel(writer, sheet_name=('Univariate_' + LoopDir))
-                AllDataMulti.to_excel(writer, sheet_name=('Multivariate_' + LoopDir)) 
-        
+                AllDataMulti.to_excel(writer, sheet_name=('Multivariate_' + LoopDir))
+
         print('')
         print('***************************************')
-        print('Succesfully saved ' + LoopDir + ' to file: ' + BehavioralSettings[3])
+        print('Successfully saved ' + LoopDir + ' to file: ' + BehavioralSettings[3])
         print('***************************************')
-
-
-
-
